916526f5a43806a5526a0786e6367fdafe830c2b
Sorumond <sorumond@gmail.com>
2019-06-24 17:35:02 +0300

    add buttons next month, previous month and current month

--- src/calendar/calendar.jsx
+++ src/calendar/calendar.jsx
@@ -5,38 +5,73 @@ const daysInMonth = function (date) {
     return 32 - new Date(date.getFullYear(), date.getMonth(), 32).getDate();
 };
 
-export function Calendar() {
-    const calendarDays = createCalendar(2019, 6);
-  return (<div className="calendar">
-      <div className="calendar__header">
-          <div className="calendar__days">пн</div>
-          <div className="calendar__days">вт</div>
-          <div className="calendar__days">ср</div>
-          <div className="calendar__days">чт</div>
-          <div className="calendar__days">пт</div>
-          <div className="calendar__days">сб</div>
-          <div className="calendar__days">нд</div>
-      </div>
-      <div className="calendar__body">
-          {calendarDays.map((day, i) => {
-              return day ? <div className={'calendar__date'} key={i}>{day}</div> : <div className={'calendar__date'}></div>
-          })}
-      </div>
-  </div>)
-};
+export class Calendar extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {month: new Date().getMonth(),
+      year: new Date().getFullYear(),}
 
+      this.moveForward = this.moveForward.bind(this);
+      this.movePrevious = this.movePrevious.bind(this);
+      this.currentMonth = this.currentMonth.bind(this);
+    };
 
+    moveForward() {
+        this.setState(state => ({
+            month: state.month + 1
+        }))
+    }
+
+    movePrevious() {
+        this.setState(state => ({
+            month: state.month - 1
+        }))
+    }
+
+    currentMonth() {
+        this.setState(state => ({
+            month: new Date().getMonth()
+        }))
+    }
+
+    render() {
+        const calendarDays = createCalendar(this.state.year, this.state.month);
+        return (<div className="calendar">
+            <button onClick={this.movePrevious}>Попередній місяць</button>
+            <button onClick={this.currentMonth}>Сьогодні</button>
+            <button onClick={this.moveForward}>Наступний місяць</button>
+            {this.state.month + 1}
+            <div className="calendar__header">
+                <div className="calendar__dayOfWeek">пн</div>
+                <div className="calendar__dayOfWeek">вт</div>
+                <div className="calendar__dayOfWeek">ср</div>
+                <div className="calendar__dayOfWeek">чт</div>
+                <div className="calendar__dayOfWeek">пт</div>
+                <div className="calendar__dayOfWeek">сб</div>
+                <div className="calendar__dayOfWeek">нд</div>
+            </div>
+            <div className="calendar__body">
+                {calendarDays.map((day, i) => {
+                    return day ? <div className={'calendar__day'} key={i}>
+                        <div className={'calendar__date'}>{day}</div>
+                        <div className={'calendar__event'}></div>
+                    </div> : <div className={'calendar__day'}></div>
+                })}
+            </div>
+        </div>)
+    };
+}
 
 let monthName = ["Січень", "Лютий", "Березнь", "Квітень", "Травень", "Червень", "Липень", "Серпень", "Вересень", "Жовтень", "Листопад", "Грудень"];
 
 function createCalendar(year, month) {
     let body = [];
-    let date = new Date(year, month - 1);
+    let date = new Date(year, month);
     let startPosisiton = 0;
-    if (daysInMonth(date) === 0) {
+    if (date.getDay() === 0) {
         startPosisiton = 7;
     } else {
-        startPosisiton = daysInMonth(date);
+        startPosisiton = date.getDay();
     }
     for (let i = 1; i < startPosisiton; i++) {
         body.push('');
