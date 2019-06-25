67ca60df47c8cfecf1f8726fbda473331dd3937e
Sorumond <sorumond@gmail.com>
2019-06-25 17:04:32 +0300

    add background when pop Up on

--- src/pop-up/pop-up.jsx
+++ src/pop-up/pop-up.jsx
@@ -14,20 +14,22 @@ export class PopUp extends React.Component {
     }
 
     render() {
-        return (<div className="pop-up">
-            <div className="pop-up__upper"><h2 className="pop-up__title">Введите задачу</h2>
-                <input type="text" className="pop-up__description" placeholder="Задача"
-                       value={this.state.value}
-                       onChange={this.handleChange}
-                       autoFocus={true}/>
-            </div>
-            <div className="pop-up__buttons">
-                <input type="button" className="pop-up__button pop-up__button-save close" value="Сохранить"
-                       onClick={() => {
-                           this.props.saveButton(this.state.value)
-                       }}/>
-                <input type="button" className="pop-up__button pop-up__button-cancel close" value="Отменить"
-                       onClick={this.props.cancelButton}/>
+        return (<div className={'pop-up__wrapper'}>
+            <div className="pop-up">
+                <div className="pop-up__upper"><h2 className="pop-up__title">Введите задачу</h2>
+                    <input type="text" className="pop-up__description" placeholder="Задача"
+                           value={this.state.value}
+                           onChange={this.handleChange}
+                           autoFocus={true}/>
+                </div>
+                <div className="pop-up__buttons">
+                    <input type="button" className="pop-up__button pop-up__button-save close" value="Сохранить"
+                           onClick={() => {
+                               this.props.saveButton(this.state.value)
+                           }}/>
+                    <input type="button" className="pop-up__button pop-up__button-cancel close" value="Отменить"
+                           onClick={this.props.cancelButton}/>
+                </div>
             </div>
         </div>)
     };
