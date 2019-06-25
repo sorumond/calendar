878838c00e96e02d46f5b4ea67d1c3bec2c8b0d1
Sorumond <sorumond@gmail.com>
2019-06-25 16:26:54 +0300

    Updates

---
+++ precache-manifest.eeecbc919dd9455fc30f40309e027522.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db437f808f3b718bf336218adb2edaef",
+    "url": "/calendar/index.html"
+  },
+  {
+    "revision": "71b0c7860088319e528a",
+    "url": "/calendar/static/css/main.2ea436ef.chunk.css"
+  },
+  {
+    "revision": "a36533ae65938741f383",
+    "url": "/calendar/static/js/2.4f017e5b.chunk.js"
+  },
+  {
+    "revision": "71b0c7860088319e528a",
+    "url": "/calendar/static/js/main.ff1e7151.chunk.js"
+  },
+  {
+    "revision": "70cd18d5f6522f88cc6f",
+    "url": "/calendar/static/js/runtime~main.3b46a14a.js"
+  }
+]);
