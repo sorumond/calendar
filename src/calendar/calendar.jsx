914a66ca4e5fa23a839c6a41682d67bd968ef41c
Sorumond <sorumond@gmail.com>
2019-06-25 16:47:51 +0300

    add adding task

--- src/calendar/calendar.jsx
+++ src/calendar/calendar.jsx
@@ -3,80 +3,9 @@ import './calendar.css';
 import '../pop-up/pop-up.css';
 import {PopUp} from "../pop-up/pop-up";
 
-
 const daysInMonth = function (date) {
     return 32 - new Date(date.getFullYear(), date.getMonth(), 32).getDate();
 };
-
-export class Calendar extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            month: new Date().getMonth(),
-            year: new Date().getFullYear(),
-            isModalOpened: false,
-        };
-
-        this.moveForward = this.moveForward.bind(this);
-        this.movePrevious = this.movePrevious.bind(this);
-        this.currentMonth = this.currentMonth.bind(this);
-        this.openModal = this.openModal.bind(this);
-    };
-
-    openModal() {
-        this.setState(state => ({isModalOpened: !state.isModalOpened}));
-    }
-
-    moveForward() {
-        this.setState(state => ({
-            month: state.month + 1
-        }))
-    }
-
-    movePrevious() {
-        this.setState(state => ({
-            month: state.month - 1
-        }))
-    }
-
-    currentMonth() {
-        this.setState(state => ({
-            month: new Date().getMonth()
-        }))
-    }
-
-    render() {
-        const calendarDays = createCalendar(this.state.year, this.state.month);
-        return (<div className={'main'}>
-            <div className="calendar">
-                <div className="calendar__buttons">
-                    <button onClick={this.movePrevious}>Попередній місяць</button>
-                    <button onClick={this.currentMonth}>Сьогодні</button>
-                    <button onClick={this.moveForward}>Наступний місяць</button>
-                </div>
-                {this.state.month + 1}
-                <div className="calendar__header">
-                    <div className="calendar__dayOfWeek">пн</div>
-                    <div className="calendar__dayOfWeek">вт</div>
-                    <div className="calendar__dayOfWeek">ср</div>
-                    <div className="calendar__dayOfWeek">чт</div>
-                    <div className="calendar__dayOfWeek">пт</div>
-                    <div className="calendar__dayOfWeek">сб</div>
-                    <div className="calendar__dayOfWeek">нд</div>
-                </div>
-                <div className="calendar__body">
-                    {calendarDays.map((day, i) => {
-                        return day ? <div className={'calendar__day'} key={i} onClick={this.openModal}>
-                            <div className={'calendar__date'}>{day}</div>
-                            <div className={'calendar__event'}></div>
-                        </div> : <div className={'calendar__day'}></div>
-                    })}
-                </div>
-            </div>
-            {this.state.isModalOpened ? <PopUp/> : null}
-        </div>)
-    };
-}
 
 let monthName = ["Січень", "Лютий", "Березнь", "Квітень", "Травень", "Червень", "Липень", "Серпень", "Вересень", "Жовтень", "Листопад", "Грудень"];
 
@@ -93,10 +22,146 @@ function createCalendar(year, month) {
         body.push('');
     }
     for (let i = 1; i <= daysInMonth(date); i++) {
-        body.push(i);
+        let getCurrentDay = new Date(year, month, i);
+        let date = {
+            dateNumber: i,
+            fullDate: `${getCurrentDay.getDate()}.${getCurrentDay.getMonth()}.${getCurrentDay.getFullYear()}`
+        };
+        body.push(date);
     }
     for (let i = daysInMonth(date) + startPosisiton - 1; i % 7; i++) {
         body.push('');
     }
     return body;
 }
+
+let tasks = {};
+
+function renderTasks(date) {
+    let renderTask;
+    if (tasks[date]) {
+        renderTask = tasks[date].map((task) => {
+            return <li className={'calendar__task'}>{task.title}</li>
+        })
+    } else {
+        return
+    }
+    return renderTask;
+}
+
+export class Calendar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            month: new Date().getMonth(),
+            year: new Date().getFullYear(),
+            isModalOpened: false,
+            openedDate: null,
+        };
+
+        this.moveForward = this.moveForward.bind(this);
+        this.movePrevious = this.movePrevious.bind(this);
+        this.currentMonth = this.currentMonth.bind(this);
+        this.openModal = this.openModal.bind(this);
+        this.cancelButton = this.cancelButton.bind(this);
+        this.saveButton = this.saveButton.bind(this);
+    };
+
+    openModal(openedDate) {
+        this.setState(state => ({
+            isModalOpened: true,
+            openedDate: openedDate,
+        }));
+    }
+
+    saveButton(task) {
+        this.setState(state => ({isModalOpened: false}));
+
+
+        if (task !== '') {
+            if (tasks[this.state.openedDate]) {
+                tasks[this.state.openedDate].push({title: task});
+            } else {
+                tasks[this.state.openedDate] = [];
+                tasks[this.state.openedDate].push({title: task});
+            }
+        }
+    }
+
+    cancelButton() {
+        this.setState(state => ({isModalOpened: false}));
+    }
+
+    moveForward() {
+        this.setState(state => ({
+            month: state.month + 1
+        }));
+
+        if (this.state.month > 10) {
+            this.setState(state => ({
+                month: 0,
+                year: state.year + 1
+            }));
+        }
+
+    }
+
+    movePrevious() {
+        this.setState(state => ({
+            month: state.month - 1
+        }));
+
+        if (this.state.month < 1) {
+            this.setState(state => ({
+                month: 11,
+                year: state.year - 1
+            }));
+        }
+    }
+
+    currentMonth() {
+        this.setState(state => ({
+            month: new Date().getMonth(),
+            year: new Date().getFullYear()
+        }))
+    }
+
+    render() {
+        const calendarDays = createCalendar(this.state.year, this.state.month);
+        return (<div className={'main'}>
+            <div className="calendar">
+                <div className="calendar__buttons">
+                    <button onClick={this.movePrevious}>Попередній місяць</button>
+                    <button onClick={this.currentMonth}>Сьогодні</button>
+                    <button onClick={this.moveForward}>Наступний місяць</button>
+                </div>
+                <span>Місяць: {monthName[this.state.month]}, Рік: {this.state.year}</span>
+                <div className="calendar__header">
+                    <div className="calendar__dayOfWeek">пн</div>
+                    <div className="calendar__dayOfWeek">вт</div>
+                    <div className="calendar__dayOfWeek">ср</div>
+                    <div className="calendar__dayOfWeek">чт</div>
+                    <div className="calendar__dayOfWeek">пт</div>
+                    <div className="calendar__dayOfWeek">сб</div>
+                    <div className="calendar__dayOfWeek">нд</div>
+                </div>
+                <div className="calendar__body">
+                    {calendarDays.map((day, i) => {
+                        return day ? <div className={'calendar__day'} key={i} onClick={() => {
+                            this.openModal(day.fullDate);
+                        }}>
+                            <div className={'calendar__date'}>{day.dateNumber}</div>
+                            <div className={'calendar__event'}><ol>
+                                {renderTasks(day.fullDate)}
+                            </ol></div>
+                        </div> : <div className={'calendar__day'}></div>
+                    })}
+                </div>
+            </div>
+            {this.state.isModalOpened ? <PopUp openModal={this.openModal}
+                                               cancelButton={this.cancelButton}
+                                               saveButton={this.saveButton}/> : null}
+        </div>)
+    };
+}
+
--- src/pop-up/pop-up.jsx
+++ src/pop-up/pop-up.jsx
@@ -1,19 +1,34 @@
 import React from 'react';
 import './pop-up.css';
 
-export function PopUp() {
- return (
-     <div className="pop-up">
-         <h2 className="pop-up__title">Please enter your information</h2>
-         <form action="">
-             <label> Enter e-mail
-                 <input type="email" className="pop-up__login" placeholder="Email"/>
-             </label>
+export class PopUp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
 
-             <label> Enter password
-                 <input type="password" className="pop-up__password" placeholder="Password"/>
-             </label>
-             <input type="submit" className="pop-up__button close" value="ENTER"/>
-         </form>
-     </div>)
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    render() {
+        return (<div className="pop-up">
+            <div className="pop-up__upper"><h2 className="pop-up__title">Введите задачу</h2>
+                <input type="text" className="pop-up__description" placeholder="Задача"
+                       value={this.state.value}
+                       onChange={this.handleChange}
+                       autoFocus={true}/>
+            </div>
+            <div className="pop-up__buttons">
+                <input type="button" className="pop-up__button pop-up__button-save close" value="Сохранить"
+                       onClick={() => {
+                           this.props.saveButton(this.state.value)
+                       }}/>
+                <input type="button" className="pop-up__button pop-up__button-cancel close" value="Отменить"
+                       onClick={this.props.cancelButton}/>
+            </div>
+        </div>)
+    };
 }
